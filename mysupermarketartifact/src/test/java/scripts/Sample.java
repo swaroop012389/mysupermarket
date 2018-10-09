2cf215bd09416de87eec4621372e465b52734fa1
hp <hp@192.168.2.4>
2018-10-09 16:59:58 +0530

    Second

---
+++ mysupermarketartifact/src/test/java/scripts/Sample.java
@@ -0,0 +1,98 @@
+package scripts;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+	private static final String a = null;
+	private static final String b = null;
+
+	@Test(groups = {"Re"},priority=2)
+public void testSample1() 
+{
+ System.out.println("Test 1");	
+}
+	@Test(groups = {"Reg"},priority=1,dependsOnMethods= {"testSample4","testSample3"})
+public void testSample2() 
+{
+System.out.println("Test 2");	
+}
+
+	@Parameters ({"a","b"})
+	@Test(groups = {"Re"}, priority=2)	
+public void testSample3(int a,int b) 
+{		
+System.out.println("Test 3"); 
+System.out.println(a+" "+b); 
+}
+	
+	@Test(groups = {"Reg","Re"},priority=0)
+public void testSample4() 
+{
+System.out.println("Test 4"); 	
+}
+	
+	@Test(groups = {"Reg","Re"},priority=0)
+public void testSample5() 
+{
+System.out.println("Test 5"); 	
+}
+	@AfterSuite
+	public void afterSuite() 
+	{
+	System.out.println("After Suite is executed"); 	
+	}
+	
+	@AfterTest
+	public void afterTest() 
+	{
+	System.out.println("After Test is executed"); 	
+	}
+	
+	@AfterClass
+	public void afterClass() 
+	{
+	System.out.println("After Class is executed"); 	
+	}
+
+	@AfterMethod
+	public void afterMethod() 
+	{
+	System.out.println("After Method is executed"); 	
+	}
+	
+	@BeforeSuite
+	public void beforeSuite() 
+	{
+	System.out.println("Before Suite starts"); 	
+	}
+	
+	@BeforeTest
+	public void beforeTest() 
+	{
+	System.out.println("Before Test starts"); 	
+	}
+	
+	@BeforeClass
+	public void beforeClass() 
+	{
+	System.out.println("Before Class starts"); 	
+	}
+
+	@BeforeMethod
+	public void beforeMethod() 
+	{
+	System.out.println("Before starts starts"); 	
+	}
+	
+	
+}
